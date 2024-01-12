160b274b9eeef0786b2b6901773038ff62ab81c2
vm7608 <102200270@sv1.dut.udn.vn>
2024-01-12 23:21:17 +0700

    Fix bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -93,7 +93,7 @@ def crawl_news():
             # print('Done: ', url)
         except:
             print('Error: ', url)
-        return title + time + description + image_path
+        return picture
         break
 
     # drop row with empty image_path
